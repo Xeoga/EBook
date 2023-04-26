2a4d26f4aa8ee5e4ce0f617b6ec8f5486fa6a4f2
ChiritaS_AKA_Heoga <bbelaci@mail.ru>
2023-04-26 12:23:04 +0300

    Add BusinessLogic

---
+++ BussinesLayer/SalesProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer
+{
+    internal class SalesProcessor
+    {
+        public decimal CalculateTotalPrice(decimal price, int quantity, decimal discount)
+        {
+            // Implementarea logicii de calculare a prețului total
+            decimal discountedPrice = price * (1 - discount);
+            return discountedPrice * quantity;
+        }
+
+        public decimal ApplyTax(decimal totalPrice, decimal taxRate)
+        {
+            // Implementarea logicii de aplicare a taxelor
+            return totalPrice * (1 + taxRate);
+        }
+    }
+}
---
+++ BussinesLayer/ValidationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer
+{
+    internal class ValidationHelper
+    {
+        public bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // Implementarea logicii de validare a numărului de telefon
+            // Exemplu simplu: verifică dacă are 10 caractere și conține doar cifre
+            return !string.IsNullOrEmpty(phoneNumber) && phoneNumber.Length == 10 && phoneNumber.All(char.IsDigit);
+        }
+
+        public bool IsValidEmailAddress(string emailAddress)
+        {
+            // Implementarea logicii de validare a adresei de email
+            // Exemplu simplu: verifică dacă are un format valid de email
+            return !string.IsNullOrEmpty(emailAddress) && new EmailAddressAttribute().IsValid(emailAddress);
+        }
+    }
+}
