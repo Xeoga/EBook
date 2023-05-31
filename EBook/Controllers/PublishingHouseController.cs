8fd0efed44c7b7fcc23d01a38534c8a6162b6654
ChiritaS_AKA_Heoga <bbelaci@mail.ru>
2023-05-31 13:35:47 +0300

    Return to work

---
+++ EBook/Controllers/PublishingHouseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EBook.BussinesLogic.Services;
+using EBook.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+namespace EBook.Controllers
+{
+    public class PublishingHouseController : Controller
+    {
+        //private readonly IPublishingHouse _service;
+        //public PublishingHouse(PublishingHouse service)
+        //{
+            //_service = service;
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
